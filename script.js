436f25cf616a7436948660ff8777efd5de925b12
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-08-21 19:33:59 -0400

    added vertical text

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const here = document.getElementById('here');
 const text = document.getElementById('text');
+const para = document.getElementById('para');
+const reverse = document.getElementById('reverse');
+const stacked = document.getElementById('stacked');
+const stackedReversed = document.getElementById('stackedReversed');
+const stackedSide = document.getElementById('stackedSide');
+const stackedReversedSide = document.getElementById('stackedReversedSide');
 
 text.addEventListener('keyup', function display(){
     const value = text.value;
-    const para = document.getElementById('para');
     para.innerHTML = value;
     
     let reverseText = '';
-    const reverse = document.getElementById('reverse');
+
     for (let i = value.length - 1; i > -1; i--){
         reverseText = reverseText + value[i]
     }
+
     reverse.innerHTML = reverseText;
+    stacked.innerHTML = value;
+    stackedReversed.innerHTML = reverseText;
+    stackedSide.innerHTML = value;
+    stackedReversedSide.innerHTML = reverseText;
 });
